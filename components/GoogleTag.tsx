d7e1e9e28af6f7241f37014cf5fc12d4c21bbcf1
6xfkrz24bb-sys <6xfkrz24bb@privaterelay.appleid.com>
2026-06-30 09:11:16 -0300

    Instala tag Google Ads no site

--- components/GoogleTag.tsx
+++ components/GoogleTag.tsx
@@ -1,58 +1,21 @@
 import Script from 'next/script';
 
-const DEFAULT_GOOGLE_TAG_ID = 'GTM-K895MNRB';
-
-function normalizeGoogleTagId(value?: string) {
-  const tagId = (value || DEFAULT_GOOGLE_TAG_ID).trim();
-  if (!tagId) return null;
-  return /^[A-Za-z0-9_-]+$/.test(tagId) ? tagId : null;
-}
+const GOOGLE_ADS_TAG_ID = 'AW-18130712066';
 
 export function GoogleTag() {
-  const tagId = normalizeGoogleTagId(process.env.NEXT_PUBLIC_GOOGLE_TAG_ID);
-  if (!tagId) return null;
-
-  if (tagId.startsWith('GTM-')) {
-    return (
-      <>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${tagId}');
-          `}
-        </Script>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${tagId}`}
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-      </>
-    );
-  }
-
   return (
     <>
       <Script
-        id="google-tag-loader"
-        src={`https://www.googletagmanager.com/gtag/js?id=${tagId}`}
+        id="google-ads-tag-loader"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_TAG_ID}`}
         strategy="afterInteractive"
       />
-      <Script id="google-tag-init" strategy="afterInteractive">
+      <Script id="google-ads-tag-init" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${tagId}');
+          gtag('config', '${GOOGLE_ADS_TAG_ID}');
         `}
       </Script>
     </>
